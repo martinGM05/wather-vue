02d897d76ae24f68ab1f68077243915108eb2cf3
Martin <mgonzalez@mediomelon.mx>
2023-11-18 00:20:17 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import Formulario from './components/Formulario.vue';
+
+</script>
+
+<template>
+  <h1 class="titulo">Buscador de clima</h1>
+
+  <div class="contenedor buscador-clima">
+    <Formulario />
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>

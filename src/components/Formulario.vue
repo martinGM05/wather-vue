e5fb9c8d4662f38ae71c2a89d510679caf3f4674
Martin <mgonzalez@mediomelon.mx>
2023-11-18 23:48:12 -0600

    Upload vue project with call an API

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,10 +1,15 @@
 <script setup>
-    import { reactive } from 'vue';
+    import { reactive, ref } from 'vue';
+    import Alerta from './Alerta.vue'
 
     const busqueda = reactive({
-        ciduad: '',
+        ciudad: '',
         pais: ''
     })
+
+    const error = ref('')
+
+    const emit = defineEmits(['obtener-clima'])
 
     const paises = [
         { codigo: 'US', nombre: 'Estados Unidos' },
@@ -16,23 +21,36 @@
         { codigo: 'PE', nombre: 'Perú' }
     ]
 
+    const consultarClima = () => {
+        if(Object.values(busqueda).includes('')) {
+            error.value = 'Todos los campos son obligatorios'
+            return
+        }
+
+        error.value = ''
+        emit('obtener-clima', busqueda)
+    }
+
 </script>
 
 <template>
     <form
         class="formulario"
+        @submit.prevent="consultarClima"
     >
+
+    <Alerta v-if="error">{{ error }}</Alerta>
 
     <div class="campo">
         <label for="ciudad">Ciudad</label>
-        <input type="text" id="ciudad" placeholder="Ciudad"  v-model="busqueda.ciduad">
+        <input type="text" id="ciudad" placeholder="Ciudad"  v-model="busqueda.ciudad">
     </div>
 
     <div class="campo">
         <label for="pais">País</label>
         <select id="pais" v-model="busqueda.pais">
             <option value="">-- Seleccione un país --</option>
-            <option v-for="pais in paises" value="pais.codigo" v-bind:key="pais.codigo">{{ pais.nombre }}</option>
+            <option v-for="pais in paises" :value="pais.codigo" v-bind:key="pais.codigo">{{ pais.nombre }}</option>
         </select>
     </div>
 
